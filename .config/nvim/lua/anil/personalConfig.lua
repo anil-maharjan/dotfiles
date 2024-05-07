58467922d65dce14bebceea0d2c51f709088de1e
anil-maharjan <maharjananil68@gmail.com>
2024-05-07 20:49:45 +0545

    update config files

--- .config/nvim/lua/anil/personalConfig.lua
+++ .config/nvim/lua/anil/personalConfig.lua
@@ -1,7 +1,40 @@
+local opt = vim.opt;
+local builtin = require('telescope.builtin')
+
+--opt.guicursor = ""
+opt.tabstop = 2
+opt.nu = true
+opt.relativenumber = true
+opt.softtabstop = 2
+opt.smartindent = true
+opt.wrap = false
+opt.swapfile = false
+opt.shiftwidth = 2
+opt.backup = false
+opt.expandtab = true
+opt.ignorecase = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+opt.incsearch = true
+opt.hlsearch = false
+opt.laststatus = 2
+opt.background = 'dark'
+--opt.colorcolumn = '120'
+opt.scrolloff = 10
+opt.updatetime = 100
+opt.hlsearch = false
+
 -- Full path
 vim.cmd("command! CpFpath lua vim.fn.setreg('+', vim.fn.expand('%:p'))")
 
 -- Relative path
 vim.cmd("command! CpRpath lua vim.fn.setreg('+', vim.fn.expand('%:p:~:.'))")
-
-
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>\\h', ':set hlsearch!<CR>')
+vim.keymap.set("n", "GI", "<Plug>(coc-implementation)", {silent = true})
+vim.keymap.set('n', "<leader>z", 'za', {silent = true, noremap = true})
+vim.keymap.set('n', '<leader>b', function() builtin.buffers({
+  sort_mru=true,
+  ignore_current_buffer=true
+}) end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/anil/.cache/nvim/packer_hererocks/2.1.1706185428/share/lua/5.1/?.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1706185428/share/lua/5.1/?/init.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1706185428/lib/luarocks/rocks-5.1/?.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1706185428/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/anil/.cache/nvim/packer_hererocks/2.1.1706185428/lib/lua/5.1/?.so"
+local package_path_str = "/home/anil/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1710088188/share/lua/5.1/?/init.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?.lua;/home/anil/.cache/nvim/packer_hererocks/2.1.1710088188/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/anil/.cache/nvim/packer_hererocks/2.1.1710088188/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -210,6 +210,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/anil/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/home/anil/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
   },
   ["vim-devicons"] = {
     loaded = true,
